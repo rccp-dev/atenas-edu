e0411fc58bac2437dba4d032dd5e2e3f770646f8
rccp-dev <ricardocustodio0110@gmail.com>
2026-05-10 05:27:11 -0300

    Modules

---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google"
+
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-primary",
+});
+
+export const metadata: Metadata = {
+  title: "Atenas",
+  description: "Plataforma acadêmica",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${poppins.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
