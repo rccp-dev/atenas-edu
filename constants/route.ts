e7175720ac94f0c85abdcc4bb149642b051da9fa
Ricardo Custódio Pereira <846250@rpo-acad.unaerpnet.br>
2026-05-28 09:59:07 -0300

    getSession() implementado

--- constants/route.ts
+++ constants/route.ts
@@ -1,8 +1,14 @@
-/* Finalizar */
+/* Não utilizado em nenhuma parte do código ainda */
 
 export const ROUTES = {
     LOGIN: "/login",
     HOME: "/",
+    ABOUT: "/sobre",
+    DASHBOARD: "/dashboard",
     STUDENTS: "/alunos",
     ACTIVITIES: "/atividades",
+    ACTIVITY: "/atividade",
+    SUBMISSIONS: "/envios",
+    LESSONPLANS: "/planos",
+    CLASSROOMS: "/turmas"
 };
--- proxy.ts
+++ proxy.ts
@@ -25,7 +25,9 @@ export async function proxy(request: NextRequest) {
             }
         );
 
-    const { data: { user }, } = await supabase.auth.getUser();
+    const {data: { session },} = await supabase.auth.getSession();
+
+    const user = session?.user;
 
     const isLoginPage = request.nextUrl.pathname === "/login";
 
