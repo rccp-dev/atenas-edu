61ee544b97311328d20f599310144b496e9b8fc8
Ricardo Custódio Pereira <846250@rpo-acad.unaerpnet.br>
2026-05-28 09:17:30 -0300

    Novo proxy

--- proxy.ts
+++ proxy.ts
@@ -1,33 +1,74 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 import { createServerClient } from "@supabase/ssr";
 
-export async function proxy(request: NextRequest) {
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-    const supabase = createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-        {
-            cookies: {
-                getAll: () => request.cookies.getAll(),
-                setAll: () => {}
+export async function proxy(
+    request: NextRequest
+) {
+
+    let response = NextResponse.next();
+
+    const supabase =
+        createServerClient(
+            process.env.NEXT_PUBLIC_SUPABASE_URL!,
+            process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+            {
+                cookies: {
+
+                    getAll() {
+                        return request.cookies.getAll();
+                    },
+
+                    setAll(cookiesToSet) {
+
+                        cookiesToSet.forEach(
+                            ({
+                                name,
+                                value,
+                                options,
+                            }) => {
+
+                                response.cookies.set(
+                                    name,
+                                    value,
+                                    options
+                                );
+
+                            }
+                        );
+
+                    },
+
+                },
             }
-        }
-    );
+        );
 
-    const { data: { user } } = await supabase.auth.getUser();
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
 
-    const isLoginPage = request.nextUrl.pathname === "/login";
+    const isLoginPage =
+        request.nextUrl.pathname === "/login";
 
     if (!user && !isLoginPage) {
-        return NextResponse.redirect(new URL("/login", request.url));
+
+        return NextResponse.redirect(
+            new URL("/login", request.url)
+        );
+
     }
 
     if (user && isLoginPage) {
-        return NextResponse.redirect(new URL("/", request.url));
+
+        return NextResponse.redirect(
+            new URL("/", request.url)
+        );
+
     }
 
-    return NextResponse.next();
+    return response;
+
 }
 
 export const config = {
@@ -39,6 +80,7 @@ export const config = {
         "/envios/:path*",
         "/planos/:path*",
         "/tarefas/:path*",
-        "/turmas/:path*"
+        "/turmas/:path*",
+        "/login",
     ],
 };
